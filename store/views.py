e76c80d87c4a9dfae373fcd676220e8aaba67fff
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2024-09-04 21:10:04 +0530

    Add pagination

--- store/views.py
+++ store/views.py
@@ -4,21 +4,32 @@ from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 def store(request,category_slug=None):
     categories = None
     products = None
-    # Get product by category 
+    # Get product by their category 
     if category_slug!= None :
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.all().filter(category=categories,is_available=True)
+        products = Product.objects.all().filter(category=categories,is_available=True).order_by('id')
+        #Paginage the products
+        paginator = Paginator(products, 4)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
+        
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        #Paginage the products
+        paginator = Paginator(products, 4)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context  = {
-        "products": products,
+        "products": paged_products,
         "category_name": categories,
         "product_count": product_count
     }
@@ -34,7 +45,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        'product_name': single_product.product_name.title(),
+        'product_name'  : single_product.product_name.title(),
         'in_cart'       : in_cart,
        
     }
