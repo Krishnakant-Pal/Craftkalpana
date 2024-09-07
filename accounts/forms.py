cede3e6da45d48aa19effaa3225de6c072294002
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2024-09-07 15:33:00 +0530

    add login, register, and logout functionality
    - Implemented user login functionality with authentication
    - Added user registration for new accounts
    - Set up user logout to end sessions
    - Included necessary forms and views for authentication flow

---
+++ accounts/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        '''Check if the password and confirm_password provided match'''
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password doesn't match.    "
+            )
